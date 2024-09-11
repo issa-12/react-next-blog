40d0ff2b3c44102d303ceb694aa12a14b56920f4
issa <issaalayan2017@gmail.com>
2024-09-11 22:01:40 +0300

    Delete_Blog_Feature

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -4,6 +4,7 @@
 import BlogTableItem from '@/Components/AdminComponents/BlogTableItem'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify';
 
 const page = () => {
 
@@ -12,6 +13,17 @@ const page = () => {
   const fetchBlogs  = async () => {
     const response = await axios.get('/api/blog');
     setBlogs(response.data.blogs);
+  }
+
+  const deleteBlog = async (mongoId)=> {  
+    const response = await axios.delete('/api/blog',{
+      params:
+        {
+          id: mongoId
+        }
+      })
+    toast.success(response.data.msg);
+    fetchBlogs();
   }
 
   useEffect(()=>{
@@ -44,7 +56,7 @@ const page = () => {
         </thead>
         <tbody>
           {blogs.map((item,index)=>{
-            return  <BlogTableItem key={index} mongoId={item._id} title={item.title} author={item.author} authorImg={item.authorImg} date={item.date}/>
+            return  <BlogTableItem key={index} mongoId={item._id} title={item.title} author={item.author} authorImg={item.authorImg} date={item.date} deleteBlog={deleteBlog}/>
           })}
         </tbody>
       </table>
--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -4,6 +4,7 @@ import { write } from "fs";
 
 const { NextResponse } = require("next/server")
 import {writeFile} from 'fs/promises'
+const fs = require('fs')
 
 const LoadDB = async ()=>{
     await ConnectDB();
@@ -54,4 +55,14 @@ export async function POST(request) {
     console.log("Blog Saved");
 
     return NextResponse.json({success:true,msg:"Blog Added"})
+}
+
+// Creating API Endpoint to delete Blog
+
+export async function DELETE(request) {
+    const id = await request.nextUrl.searchParams.get('id');
+    const blog = await BlogModel.findById(id);
+    fs.unlink(`./public${blog.image}`,()=>{});
+    await BlogModel.findByIdAndDelete(id);
+    return NextResponse.json({msg:"Blog Deleted"});
 }
