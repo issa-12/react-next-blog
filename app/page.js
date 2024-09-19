a428ef1f84eaa2e26da35d4e280f4f1430e3f867
issa <issaalayan2017@gmail.com>
2024-09-19 15:01:52 +0300

    modified

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+require('dotenv').config();
 
 export default function Home() {
   return (
