d339287892279cea388d4636bd34429d46e3a82b
issa <issaalayan2017@gmail.com>
2024-09-19 13:06:32 +0300

    Email subscription

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -3,10 +3,10 @@ import BlogModel from "@/lib/models/BlogModel";
 import { write } from "fs";
 
 const { NextResponse } = require("next/server")
-import {writeFile} from 'fs/promises'
+import { writeFile } from 'fs/promises'
 const fs = require('fs')
 
-const LoadDB = async ()=>{
+const LoadDB = async () => {
     await ConnectDB();
 }
 
@@ -14,17 +14,15 @@ LoadDB();
 
 // API Endpoint to get all Blogs
 export async function GET(request) {
-   
+
     const blogId = request.nextUrl.searchParams.get("id");
-    if(blogId)
-    {
+    if (blogId) {
         const blog = await BlogModel.findById(blogId);
         return NextResponse.json(blog)
     }
-    else
-    {
+    else {
         const blogs = await BlogModel.find({});
-        return NextResponse.json({blogs})
+        return NextResponse.json({ blogs })
     }
 }
 
@@ -38,23 +36,23 @@ export async function POST(request) {
     const image = formData.get('image');
     const imageByteData = await image.arrayBuffer();
     const buffer = Buffer.from(imageByteData);
-    const path =`./public/${timestamp}_${image.name}`;
-    await writeFile(path,buffer);
-    const imgUrl =`/${timestamp}_${image.name}`;
+    const path = `./public/${timestamp}_${image.name}`;
+    await writeFile(path, buffer);
+    const imgUrl = `/${timestamp}_${image.name}`;
 
-    const blogData ={
-        title:`${formData.get('title')}`,
-        description:`${formData.get('description')}`,
-        category:`${formData.get('category')}`,
-        author:`${formData.get('author')}`,
-        image:`${imgUrl}`,
-        authorImg:`${formData.get('authorImg')}`
+    const blogData = {
+        title: `${formData.get('title')}`,
+        description: `${formData.get('description')}`,
+        category: `${formData.get('category')}`,
+        author: `${formData.get('author')}`,
+        image: `${imgUrl}`,
+        authorImg: `${formData.get('authorImg')}`
     }
 
     await BlogModel.create(blogData);
     console.log("Blog Saved");
 
-    return NextResponse.json({success:true,msg:"Blog Added"})
+    return NextResponse.json({ success: true, msg: "Blog Added" })
 }
 
 // Creating API Endpoint to delete Blog
@@ -62,7 +60,7 @@ export async function POST(request) {
 export async function DELETE(request) {
     const id = await request.nextUrl.searchParams.get('id');
     const blog = await BlogModel.findById(id);
-    fs.unlink(`./public${blog.image}`,()=>{});
+    fs.unlink(`./public${blog.image}`, () => { });
     await BlogModel.findByIdAndDelete(id);
-    return NextResponse.json({msg:"Blog Deleted"});
+    return NextResponse.json({ msg: "Blog Deleted" });
 }
